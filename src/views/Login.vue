cd76b4e7184e2e159fac7783dc2c2d9ce7a42f53
Pharanya <pharanya_li@cmu.ac.th>
2023-10-19 16:26:31 +0700

    link log in to home page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,3 @@
-
-import type router from '@/router';
-
-import type router from '@/router';
 <template>
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -11,24 +7,25 @@ import type router from '@/router';
 
     <!-- username-->
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
-      <div>
-        <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-        <div class="mt-2">
-        <input id="username" name="username" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+      <form class="space-y-6" @submit.prevent="login">
+        <div>
+          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+          <div class="mt-2">
+            <input id="username" v-model="formData.username" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
         </div>
-      </div>
 
+        <!-- password -->
         <div>
           <div class="flex items-center justify-between">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           </div>
           <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="password" v-model="formData.password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
-
         </div>
 
+        <!-- Login button -->
         <div>
           <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
         </div>
@@ -43,9 +40,24 @@ import type router from '@/router';
   </div>
 </template>
 
-<style scoped>
-/* .input {
-padding: 10px;
-} */
-
-</style>
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    login() {
+      if (!this.formData.username || !this.formData.password) {
+        alert('Please fill in both the username and password fields.');
+      } else {
+        this.$router.push('/');
+      }
+    },
+  },
+};
+</script>
