b4c873a7c28e5ff775a8f7838defa26031f94993
Pharanya <pharanya_li@cmu.ac.th>
2023-10-19 16:40:40 +0700

    link login and check password

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,26 +1,25 @@
-
-import type router from '@/router';
 <template>
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img class="mx-auto h-60 w-60" src='../assets/logo.png' alt="logo" />
-      <h2 class="mt-3 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register your accouct</h2>
+      <h2 class="mt-3 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register</h2>
     </div>
 
-    <!-- username-->
+    <!-- username -->
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
-      <div>
-        <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-        <div class="mt-2">
-        <input id="username" name="username" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+      <form class="space-y-6" @submit.prevent="signup">
+        <div>
+          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+          <div class="mt-2">
+            <input id="username" v-model="formData.username" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
         </div>
-      </div>
 
+        <!-- email -->
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="email" v-model="formData.email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
@@ -30,20 +29,21 @@ import type router from '@/router';
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           </div>
           <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="password" v-model="formData.password" type="password" autocomplete="new-password" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
-        <!--confirm password -->
+        <!-- repeat password -->
         <div>
           <div class="flex items-center justify-between">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Repeat Password</label>
           </div>
           <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="passwordRepeat" v-model="formData.passwordRepeat" type="password" autocomplete="new-password" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
+        <!-- Sign up button -->
         <div>
           <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
         </div>
@@ -52,8 +52,34 @@ import type router from '@/router';
       <p class="mt-10 text-center text-sm text-gray-500">
         Already have an account?
         {{ ' ' }}
-        <a href="/Login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Log in</a>
+        <RouterLink to="/Login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Log in</RouterLink>
       </p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        username: '',
+        email: '',
+        password: '',
+        passwordRepeat: '',
+      },
+    };
+  },
+  methods: {
+    signup() {
+      if (!this.formData.username || !this.formData.email || !this.formData.password || !this.formData.passwordRepeat) {
+        alert('Please fill in all required fields.');
+      } else if (this.formData.password !== this.formData.passwordRepeat) {
+        alert('Passwords do not match. Please check and try again.');
+      } else {
+        this.$router.push('/Login');
+      }
+    },
+  },
+};
+</script>
