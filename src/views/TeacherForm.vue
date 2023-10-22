3b042cc3f38e75f3db7f5470493a12fd16eac0aa
danaikrit <danaikrit_j@cmu.ac.th>
2023-10-22 16:29:54 +0700

    edit teacherform

--- src/views/TeacherForm.vue
+++ src/views/TeacherForm.vue
@@ -2,73 +2,46 @@
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img class="mx-auto h-60 w-60" src='../assets/logo.png' alt="logo" />
-      <h2 class="mt-3 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Teacher Form</h2>
     </div>
 
-    <!-- username -->
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" @submit.prevent="signup">
-        <div>
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+    <link
+    href="https://fonts.googleapis.com/css2?family=Caveat:wght@600&family=Noto+Sans+SC:wght@700&family=Oxygen&family=Plus+Jakarta+Sans:wght@300&family=Racing+Sans+One&family=Raleway:wght@100;200;700&family=Roboto+Slab:wght@500;700&family=Rowdies:wght@300&family=Rubik:wght@500&display=swap"
+    rel="stylesheet">
+  <div class="form">
+      <div class="form-section">
+        <div class="from">
+          <h1 class="font-bold text-3xl mb-4">Add new teacher form</h1>
+      <form @submit.prevent="signup">
+        <!-- firstname -->
           <div class="mt-2">
-            <input id="firstname" v-model="formData.firstname" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="firstname" v-model="formData.firstname" type="text" autocomplete="username" required placeholder="Teacher Firstname" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
-        </div>
-        <div>
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+           <!-- lastname -->
           <div class="mt-2">
-            <input id="lastname" v-model="formData.lastname" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="lastname" v-model="formData.lastname" type="text" autocomplete="username" required placeholder="Teacher Lastname" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
-        </div>
-
-        <div>
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">User name</label>
+           <!-- username -->
           <div class="mt-2">
-            <input id="username" v-model="formData.username" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="username" v-model="formData.username" type="text" autocomplete="username" required placeholder="Teacher Username" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
-        </div>
-
-        <!-- email -->
-        <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+           <!-- email -->
           <div class="mt-2">
-            <input id="email" v-model="formData.email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="email" v-model="formData.email" type="email" autocomplete="email" required placeholder="Teacher Email" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
-        </div>
-
-        <!-- password -->
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-          </div>
+           <!-- password -->
           <div class="mt-2">
-            <input id="password" v-model="formData.password" type="password" autocomplete="new-password" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="password" v-model="formData.password" type="password" autocomplete="new-password" required placeholder="Teacher Password" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
-        </div>
-
-        <!-- repeat password -->
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
-          </div>
+           <!-- repeat -->
           <div class="mt-2">
-            <input id="passwordRepeat" v-model="formData.passwordRepeat" type="password" autocomplete="new-password" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="passwordRepeat" v-model="formData.passwordRepeat" type="password" autocomplete="new-password" required placeholder="Repeat Password" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
-        </div>
-
-        <!-- Sign up button -->
-        <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
-        </div>
+        <button class="button-19 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" id="button-19-teacher" type="submit">Add Teacher</button>
       </form>
-
-      <p class="mt-10 text-center text-sm text-gray-500">
-        Already have an account?
-        {{ ' ' }}
-        <RouterLink to="/Login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Log in</RouterLink>
-      </p>
+      </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -79,7 +52,7 @@ export default {
     return {
       formData: {
         firstname: '',
-        lastname:' ',
+        lastname: '',
         username: '',
         email: '',
         password: '',
@@ -130,3 +103,130 @@ export default {
 };
 
 </script>
+<style scoped>
+    .form {
+      font-family: 'Roboto', sans-serif;
+      padding: 20px;
+    }
+    
+    #formSelect {
+      margin: 10px 0;
+      padding: 8px;
+      font-size: 16px;
+      border-radius:20px ;
+    
+    }
+    
+    .form-section {
+      margin-top: 20px;
+      padding: 20px;
+      border: 1px solid #ccc;
+      /* background-color: #9dcdff; */
+      background-image: url(https://tinyurl.com/52pvsnnd);
+      border-radius: 10px;
+      box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+      color:#5f5f5f;
+      
+    }
+    
+    input {
+      width: 100%;
+      padding: 10px;
+      margin: 5px 0;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+    
+    #button-19-student{
+      margin: auto;
+      display: block;
+      margin-top: 20px;
+    }
+
+    #button-19-teacher{
+      margin: auto;
+      display: block;
+      margin-top: 20px;
+    }
+      .button-19 {
+        appearance: button;
+      background-color: #b062b7;
+      border: solid transparent;
+      border-radius: 16px;
+      border-width: 0 0 4px;
+      box-sizing: border-box;
+      color: #FFFFFF;
+      cursor: pointer;
+      display: inline-block;
+      font-family: din-round,sans-serif;
+      font-size: 15px;
+      margin-left: 20px;
+      margin-top: 20px;
+      font-weight: 700;
+      letter-spacing: .8px;
+      line-height: 20px;
+      outline: none;
+      overflow: visible;
+      padding: 13px 16px;
+      text-align: center;
+      text-transform: uppercase;
+      touch-action: manipulation;
+      transform: translateZ(0);
+      transition: filter .2s;
+      user-select: none;
+      -webkit-user-select: none;
+      vertical-align: middle;
+      white-space: nowrap;
+      width: 40%;
+      transition: transform 0.2s, rotate 0.2s;
+    }
+    
+    .button-19:after {
+      background-clip: padding-box;
+      background-color: #cf6ed7;
+      border: solid transparent;
+      border-radius: 16px;
+      border-width: 0 0 4px;
+      bottom: -4px;
+      content: "";
+      left: 0;
+      position: absolute;
+      right: 0;
+      top: 0;
+      z-index: -1;
+    
+    }
+    
+    .button-19:main,
+    .button-19:focus {
+      outline: 2px solid #4F46E5;
+      user-select: auto;
+      outline-offset: 4px
+    }
+    
+    .button-19:hover:not(:disabled) {
+      filter: brightness(1.1);
+      -webkit-filter: brightness(1.1);
+      transform: rotate(2deg) scale(1.1);
+    }
+    
+    .button-19:disabled {
+      cursor: auto;
+    }
+    h1 {
+      font-family: 'Plus Jakarta Sans', sans-serif;
+    }
+    
+    @media (min-width: 1024px) {
+      /* .form {
+        margin-top: 100px;
+      } */
+      input{
+        width: 100%;
+      }
+      #button-19-student{
+        margin-left: 310px;
+      }
+    }
+    
+  </style>
