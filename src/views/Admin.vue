50119570f210e1f9e2ee6b95f49af0939a0cda52
Pharanya <pharanya_li@cmu.ac.th>
2023-10-23 18:02:37 +0700

    renovate admin page

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,60 +1,79 @@
 <template>
   <main class="container">
     <span class="block text-center rounded-t-lg p-3 font-semibold text-lg bg-[url('https://tinyurl.com/4zf8nrhf')]">
-      All Advisors
+      All Advisor
     </span>
-    <div class="bg-white p-6 rounded-b-lg shadow-md space-y-4">
-      <!-- Table -->
-      <table class="w-full">
+
+    <div class="content">
+      <table class="w-full bg-white">
         <thead>
           <tr>
-            <th class="text-left font-bold">Picture</th>
-            <th class="text-left font-bold">AdvisorID</th>
-            <th class="text-left font-bold">Name</th>
-            <th class="text-left font-bold">Department</th>
+            <th class="text-center font-bold p-3"> </th>
+            <th class="text-left font-bold p-3">Name</th>
+            <th class="text-left font-bold p-3">Academic Position</th>
+            <th class="text-left font-bold p-3">Department</th>
           </tr>
         </thead>
         <tbody>
-          <advisor-row v-for="(advisor, index) in advisors" :key="index" :advisor="advisor" />
+          <tr v-for="(profile, index) in profiles" :key="index">
+            <td class="flex justify-center items-center p-3">
+              <img :src="profile.image" class="w-24 h-24 object-cover rounded-full shadow-lg" alt="Profile Image" />
+            </td>
+            <td class="p-3">
+              <div class="text-lg">{{ profile.name }} {{ profile.surname }}</div>
+            </td>
+            <td class="p-3">
+              <div class="text-lg">{{ profile.academicPosition }}</div>
+            </td>
+            <td class="p-3">
+              <div class="text-lg">{{ profile.department }}</div>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
   </main>
 </template>
 
-<script>
-// import AdvisorRole from './AdvisorRole.vue';
 
-export default {
-  data() {
-    return {
-      advisors: [
-        {
-          name: "Advisor 1",
-          picture: "../assets/pic1.jpg",
-          id: "AD001",
-          department: "Department A",
-          advisees: [
-            { id: "Stud001", name: "Student A" },
-            { id: "Stud002", name: "Student B" },
-          ],
-        },
-        {
-          name: "Advisor 2",
-          picture: "../assets/pic2.jpg",
-          id: "AD002",
-          department: "Department B",
-          advisees: [
-            { id: "Stud003", name: "Student C" },
-            { id: "Stud004", name: "Student D" },
-        ]
-          },
-        // Add more advisors with their data
-      ],
-    };
+<script setup>
+
+const profiles = [
+  {
+    id: 1,
+    name: "Wojciech",
+    surname: "Smith",
+    image: "https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?q=65&auto=format&w=2270&ar=2:1&fit=crop",
+    academicPosition: "Professor",
+    department: "Computer Science",
   },
-  components: {
-    // AdvisorRole,
+  {
+    id: 2,
+    name: "Maria",
+    surname: "Johnson",
+    image: "https://preview.redd.it/r3lcc7gf64791.png?width=540&format=png&auto=webp&s=9d32ec46f884486fd59dfd01453bf5d07bf30d75",
+    academicPosition: "Assistant Professor",
+    department: "Biology",
   },
+  {
+    id: 3,
+    name: "Anna",
+    surname: "Williams",
+    image: "https://media.istockphoto.com/id/1434414228/photo/stern-sad-cat-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=wis7NGP0_4_Vwti3xioilh3NfYrwHO-JoRMvFVzr6Ck=",
+    academicPosition: "Associate Professor",
+    department: "Physics",
+  },
+];
+
+const sortAsc = () => {
+  profiles.sort((a, b) => a.likes - b.likes);
+};
+
+const sortDesc = () => {
+  profiles.sort((a, b) => b.likes - a.likes);
 };
 </script>
+
+<style>
+/* Your existing CSS styles here */
+</style>
