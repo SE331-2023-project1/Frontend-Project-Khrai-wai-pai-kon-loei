04aa6fed22a6029f25969fc8194b6a44c0341ab4
Pharanya <pharanya_li@cmu.ac.th>
2023-10-30 22:37:05 +0700

    edit  create annocement

--- src/views/announcement/CreateAnnouncementView.vue
+++ src/views/announcement/CreateAnnouncementView.vue
@@ -6,7 +6,7 @@
       </h1>
       <form>
         <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
-          <div class="grid gap-4 gap-y-2 text-sm grid-cols-1">
+          <div class="grid gap-8 gap-y-8 text-sm grid-cols-1">
             <div>
               <label for="title">Title</label>
               <InputText
