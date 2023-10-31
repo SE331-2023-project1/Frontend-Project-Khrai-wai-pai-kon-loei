15908635ba8e3882d1cc15d651c0266c1346a73f
Pattanachai_n <pondlala33@gmail.com>
2023-10-31 23:59:05 +0700

    edit messagestore

--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -11,6 +11,7 @@ import { useMessageStore } from "@/stores/message";
 const studentStore = useStudentAllStore();
 const isEdit = ref(false);
 const student = ref<Student | null>(null);
+const messageStore = useMessageStore()
 
 const userId = localStorage.getItem("user_id") as string;
 
@@ -54,9 +55,18 @@ const onSubmit = handleSubmit(async (values) => {
       values.email,
     );
     storeMessage.updateMessage("Update profile successful");
-    setTimeout(() => {
-      storeMessage.resetMessage();
-    }, 4000);
+    const svgPath = `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="currentColor" class="h-10 w-10">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+    `
+    messageStore.updateSvgPath(svgPath)
+      setTimeout(() => {
+        messageStore.resetMessage(),
+        messageStore.resetSubmessage(),
+        messageStore.resetSvgPath()
+      }, 5000)
+    
   } catch (error) {
     storeMessage.updateMessage("Could not update profile");
     setTimeout(() => {
